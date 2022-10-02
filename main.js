8fcf5a0383e44dbd3a36fa06b9b9b4b056eb2d32
Mai-Adel1 <mai196079@feng.bu.edu.eg>
2022-10-02 13:27:08 -0700

    modify some details

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 function userValidation(){
-    nameregex=/[A-Za-z]/;
+    nameregex=/[A-Za-z]{6,}/;
     userName=document.getElementById('name').value;
     if(nameregex.test(userName)==0){
         document.getElementById('pl').style.color='red';
-        document.getElementById('pl').innerHTML='Please enter letters incude lower or upper case ';
+        document.getElementById('pl').innerHTML='Please enter at least 6 letters including this first lower or upper case ';
+        return false;
     }else{
         document.getElementById('pl').style.color='green';
         document.getElementById('pl').innerHTML='correct';
+        return true;
     }
 }
 
@@ -16,10 +18,20 @@ function passValidation(){
     if(regex.test(myPass)==0){
         document.getElementById('passv').style.color='red';
         document.getElementById('passv').innerHTML='Please include letters in lower or upper case';
+        return false;
     }else{
         document.getElementById('passv').style.color='green';
         document.getElementById('passv').innerHTML='✓✓'
+        return true;
     }
 
 }
+
+document.getElementById("register").onsubmit = function() {
+    if(userValidation()===false || passValidation()===false){
+        return false;
+    }
+    return true;
+}
+
 
